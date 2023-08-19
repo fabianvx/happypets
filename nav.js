796b732765da8a72d12bc2d6802d38b6ed43f849
fabianvx <134346430+fabianvx@users.noreply.github.com>
2023-08-18 18:57:30 -0600

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,164 @@
+/**
+   * Menu NavBar
+   */
+function openNav(){
+    document.getElementById("mobile-menu").style.width = "100%";
+}
+
+function closeNav(){
+    document.getElementById("mobile-menu").style.width = "0%";
+}
+
+
+
+
+function calcularEdad(fechaNacimiento) {
+  var fechaProcesada = new Date(fechaNacimiento);
+  
+  var fechaActual = new Date();
+  var edad = document.getElementById("edad");
+
+  var years = fechaActual.getFullYear() - fechaProcesada.getFullYear();
+  var months = fechaActual.getMonth() - fechaProcesada.getMonth();
+  
+  if (months < 0 || (months === 0 && fechaActual.getDate() < fechaProcesada.getDate()+1)) {
+    years--;
+  }
+  edad.value = years;
+ 
+  if(years<=10 && years>=0){
+
+    alert("Edad minima debe de ser de 10 años");
+    return;
+  }
+
+}
+
+
+
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 10.01688810481482, lng: -84.2130290973807 },  
+    zoom: 9,
+  });
+  
+  infoWindow = new google.maps.InfoWindow();
+
+  const locationButton = document.createElement("button");
+
+  locationButton.textContent = "Ver mi ubicacion";
+  locationButton.classList.add("custom-map-control-button");
+  map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+  locationButton.addEventListener("click", () => {
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+
+          infoWindow.setPosition(pos);
+          infoWindow.setContent("Ubicacion encontrada");
+          infoWindow.open(map);
+          map.setCenter(pos);
+        },
+        () => {
+          handleLocationError(true, infoWindow, map.getCenter());
+        },
+      );
+    } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, infoWindow, map.getCenter());
+    }
+  });
+}
+//Manejo de Errores
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(
+    browserHasGeolocation
+      ? "Error: The Geolocation service failed."
+      : "Error: Your browser doesn't support geolocation.",
+  );
+  infoWindow.open(map);
+}
+
+window.initMap = initMap;
+
+
+function calcularDistancia() {
+  // Obtener las coordenadas de los dos puntos
+  var punto1 = new google.maps.LatLng(9.936266379263435, -84.09252243680388); // San jose
+  var punto2 = new google.maps.LatLng(10.016841998823672, -84.21333899160999); // Alajuela
+  // Calcular la distancia entre los dos puntos en metros
+  var distancia = google.maps.geometry.spherical.computeDistanceBetween(punto1, punto2);
+
+  // Mostrar la distancia en la página
+  var distanciaKM = (distancia/1000).toFixed(2);
+  var resultado = document.getElementById("resultado");
+  resultado.innerHTML = "La distancia entre San Jose y Alajuela es de " + distanciaKM + " km.";
+}
+
+/**
+   * Porfolio isotope and filter
+   */
+window.addEventListener('load', () => {
+    let portfolioContainer = select('.portfolio-container');
+    if (portfolioContainer) {
+      let portfolioIsotope = new Isotope(portfolioContainer, {
+        itemSelector: '.portfolio-item'
+      });
+
+      let portfolioFilters = select('#portfolio-flters li', true);
+
+      on('click', '#portfolio-flters li', function(e) {
+        e.preventDefault();
+        portfolioFilters.forEach(function(el) {
+          el.classList.remove('filter-active');
+        });
+        this.classList.add('filter-active');
+
+        portfolioIsotope.arrange({
+          filter: this.getAttribute('data-filter')
+        });
+      }, true);
+    }
+
+  });
+
+  /**
+   * Initiate portfolio lightbox 
+   */
+  const portfolioLightbox = GLightbox({
+    selector: '.portfolio-lightbox'
+  });
+
+  /**
+   * Portfolio details slider
+   */
+  new Swiper('.portfolio-details-slider', {
+    speed: 400,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    }
+  });
+
+  /**
+   * Initiate portfolio details lightbox 
+   */
+  const portfolioDetailsLightbox = GLightbox({
+    selector: '.portfolio-details-lightbox',
+    width: '90%',
+    height: '90vh'
+  });
+
